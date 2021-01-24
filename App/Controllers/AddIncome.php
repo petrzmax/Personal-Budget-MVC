d730678944170ee982d37f4ab6dc058b22f6f71f
Artur Petrzak <petrzmax@gmail.com>
2021-01-24 20:28:22 +0100

    Create AddIncome controller

---
+++ App/Controllers/AddIncome.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+use \App\Models\Income;
+
+/**
+ * Income controller
+ *
+ * PHP version 7.0
+ */
+class AddIncome extends Authenticated
+{
+
+    /**
+     * Show the income page
+     *
+     * @return void
+     */
+    public function newAction()
+    {
+        View::renderTemplate('Income/new.html', [
+            'categories' => Income::getCategories()
+        ]);
+    }
+
+    /**
+     * Save income
+     *
+     * @return void
+     */
+    public function createAction()
+    {
+        $income = new Income($_POST);
+
+        $income->save();
+    }
+}
