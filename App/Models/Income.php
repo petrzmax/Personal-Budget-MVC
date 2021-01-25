559f0d98cef066cf8a2e9618a8ea561a691316a5
Artur Petrzak <petrzmax@gmail.com>
2021-01-25 03:43:36 +0100

    Save income to db

--- App/Models/Income.php
+++ App/Models/Income.php
@@ -76,6 +76,34 @@ class Income extends \Core\Model
     } 
     
     /**
+     * Save the income model with the current property values
+     *
+     * @return boolean  True if the income was saved, false otherwise
+     */
+    public function save()
+    {
+        $this->validate();
+
+        if(empty($this->errors)) {
+
+            $db = static::getDB();
+            $sql = 'INSERT INTO incomes
+                    VALUES (NULL, :user_id, :income_category_assigned_to_user_id, :incomeValue, :incomeDate, :comment)';
+            $stmt = $db->prepare($sql);
+            
+            $stmt->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+            $stmt->bindValue(':income_category_assigned_to_user_id', $this->categoryId, PDO::PARAM_INT);
+            $stmt->bindValue(':incomeValue', $this->valueInput, PDO::PARAM_STR);
+            $stmt->bindValue(':incomeDate', $this->dateInput, PDO::PARAM_STR);
+            $stmt->bindValue(':comment', htmlspecialchars($this->comment), PDO::PARAM_STR);
+            
+            return $stmt->execute();
+        }
+
+        return false;
+    }
+
+    /**
      * Validate given date
      *
      * @return boolean  True if the date is correct, false otherwise
