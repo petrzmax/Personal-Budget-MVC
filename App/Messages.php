bf497cfae5c1a77dcebc5ac2e00539456831ae11
Artur Petrzak <petrzmax@gmail.com>
2021-01-26 23:55:13 +0100

    Create Messages class

---
+++ App/Messages.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+/**
+ * Messages definitions
+ *
+ * PHP version 7.0
+ */
+class Messages
+{
+
+    /**
+     * Messages
+     * @var string
+     */
+    const LOGIN_SUCCESS = 'Zalogowano pomyślnie';
+    const LOGIN_FAIL = 'Logowanie nieudane, spróbuj ponownie';
+    const LOGOUT_SUCCESS = 'Wylogowano pomślnie';
+    const INCOME_ADD_SUCCESS = 'Przychód dodany pomyślnie';
+    const INCOME_ADD_FAIL = 'Dodawanie przychodu nieudane, spróbuj ponownie';
+    const EXPENSE_ADD_SUCCESS = 'Wydatek dodany pomyślnie';
+    const EXPENSE_ADD_FAIL = 'Dodawanie wydatku nieudane, spróbuj ponownie';
+    const CHANGES_SAVED = 'Zmiany zapisane';
+    const BALANCE_BAD_DATA = 'Podano nieprawidłowe dane';
+
+}
