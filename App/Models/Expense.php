94218f85ec379a9b2ba35e581477582dda535da9
Artur Petrzak <petrzmax@gmail.com>
2021-02-18 14:49:56 +0100

    Refactor saving category method

--- App/Models/Expense.php
+++ App/Models/Expense.php
@@ -86,33 +86,4 @@ class Expense extends Finance
 
         return $stmt->execute();
     } 
-
-    /**
-     * Save the expense model with the current property values
-     *
-     * @return boolean  True if the expense was saved, false otherwise
-     */
-    public function save()
-    {
-        $this->validate();
-
-        if(empty($this->errors)) {
-
-            $db = static::getDB();
-            $sql = 'INSERT INTO expenses
-                    VALUES (NULL, :user_id, :expense_category_assigned_to_user_id, :payment_method_assigned_to_user_id, :expenseValue, :expenseDate, :comment)';
-            $stmt = $db->prepare($sql);
-
-            $stmt->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
-            $stmt->bindValue(':expense_category_assigned_to_user_id', $this->categoryId, PDO::PARAM_INT);
-            $stmt->bindValue(':payment_method_assigned_to_user_id', $this->methodId, PDO::PARAM_INT);
-            $stmt->bindValue(':expenseValue', $this->valueInput, PDO::PARAM_STR);
-            $stmt->bindValue(':expenseDate', $this->dateInput, PDO::PARAM_STR);
-            $stmt->bindValue(':comment', htmlspecialchars($this->comment), PDO::PARAM_STR);
-            
-            return $stmt->execute();
-        }
-
-        return false;
-    }
 }
