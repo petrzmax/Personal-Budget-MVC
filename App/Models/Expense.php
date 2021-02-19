9712005c36703a327d3f1b9c35cf67829e903a09
Artur Petrzak <petrzmax@gmail.com>
2021-02-19 18:43:59 +0100

    Add get current month Sum by id method

--- App/Models/Expense.php
+++ App/Models/Expense.php
@@ -86,4 +86,39 @@ class Expense extends Finance
 
         return $stmt->execute();
     } 
+
+    /**
+     * Get expense data by id
+     *
+     * @return mixed decimal(8,2) if found, false otherwise
+     */
+    public static function getCategoryCurrentMonthSumById($id)
+    {
+        $db = static::getDB();
+
+        $startDate = date('Y-m-01');
+        $endDate = date("Y-m-t");
+
+        $sql = 'SELECT SUM(amount) AS categorySum
+            FROM expenses AS e, expenses_category_assigned_to_users AS ecu
+            WHERE e.expense_category_assigned_to_user_id = ecu.id AND ecu.id = :categoryId
+            AND e.user_id = :user_id AND date_of_expense BETWEEN :start_date AND :end_date';
+        
+        $stmt = $db->prepare($sql);
+
+        $stmt->bindValue(':categoryId', $id, PDO::PARAM_INT);
+        $stmt->bindValue(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+        $stmt->bindValue(':start_date', $startDate, PDO::PARAM_STR);
+        $stmt->bindValue(':end_date', $endDate, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        $result = $stmt->fetchColumn();
+
+        if(is_null($result)) {
+            return false;
+        }
+
+        return $result;
+    } 
 }
